c9a7c5f739fa0d1f6aaab9ca8374be9a217afb0b
shuntaaaa247 <juntairenzu3@gmail.com>
2025-08-09 01:31:04 +0900

    色々いじってみた

--- src/components/flow/index.tsx
+++ src/components/flow/index.tsx
@@ -2,6 +2,7 @@
 import { useState, useCallback } from 'react';
 import { 
   ReactFlow, 
+  MiniMap,
   applyNodeChanges, 
   applyEdgeChanges, 
   addEdge,
@@ -17,7 +18,7 @@ const initialNodes: Node[] = [
   { id: 'n1', position: { x: 0, y: 0 }, data: { label: 'Node 1' } },
   { id: 'n2', position: { x: 0, y: 100 }, data: { label: 'Node 2' } },
 ];
-const initialEdges: Edge[] = [{ id: 'n1-n2', source: 'n1', target: 'n2' }];
+const initialEdges: Edge[] = [{ id: 'n1-n2', source: 'n1', target: 'n2', type: 'smoothstep' }];
  
 export default function App() {
   const [nodes, setNodes] = useState<Node[]>(initialNodes);
@@ -32,12 +33,27 @@ export default function App() {
     [],
   );
   const onConnect = useCallback(
-    (params: Connection) => setEdges((edgesSnapshot) => addEdge(params, edgesSnapshot)),
+    (params: Connection) => setEdges((edgesSnapshot) => addEdge({ ...params, type: 'smoothstep' }, edgesSnapshot)),
     [],
   );
- 
+
+  const addNode = useCallback(() => {
+    setNodes([...nodes, { id: `n${nodes.length + 1}`, position: { x: 0, y: nodes.length * 100 }, data: { label: `Node ${nodes.length + 1}` } }]);
+  }, [nodes]);
+
+  const nodeColor = (node: Node) => {
+    switch (node.type) {
+      case 'input':
+        return '#6ede87';
+      case 'output':
+        return '#6865A5';
+      default:
+        return '#ff0072';
+    }
+  };
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
+      <button onClick={addNode}>Add Node</button>
       <ReactFlow
         nodes={nodes}
         edges={edges}
@@ -46,6 +62,7 @@ export default function App() {
         onConnect={onConnect}
         fitView
       />
+      {/* <MiniMap nodeColor={nodeColor} nodeStrokeWidth={3} zoomable pannable/> */}
     </div>
   );
 }
